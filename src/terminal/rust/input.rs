f057c2e2bd17440fb6dce22ef5140523e7e55508
Frank Wang <1454884738@qq.com>
2026-05-25 22:18:29 -0700

    refactor(vt): split Rust mouse encoder bool setter

--- src/terminal/rust/input.rs
+++ src/terminal/rust/input.rs
@@ -36,34 +36,6 @@ pub unsafe extern "C" fn ghostty_rust_mouse_encoder_setopt_format(
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn ghostty_rust_mouse_encoder_setopt_bool(
-    option: c_int,
-    value: bool,
-    any_button_pressed: *mut bool,
-    track_last_cell: *mut bool,
-    last_cell_present: *mut bool,
-) {
-    unsafe {
-        match option {
-            MOUSE_ENCODER_OPT_ANY_BUTTON_PRESSED => {
-                if !any_button_pressed.is_null() {
-                    ptr::write(any_button_pressed, value);
-                }
-            }
-            MOUSE_ENCODER_OPT_TRACK_LAST_CELL => {
-                if !track_last_cell.is_null() {
-                    ptr::write(track_last_cell, value);
-                }
-                if !value && !last_cell_present.is_null() {
-                    ptr::write(last_cell_present, false);
-                }
-            }
-            _ => {}
-        }
-    }
-}
-
-#[no_mangle]
 pub unsafe extern "C" fn ghostty_rust_mouse_encoder_from_terminal(
     event: c_int,
     format: c_int,
