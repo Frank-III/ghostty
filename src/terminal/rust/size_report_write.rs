19e41bbad53d3fa629b67fdbfa4e7c837d34bf24
Frank Wang <1454884738@qq.com>
2026-05-26 00:04:41 -0700

    refactor(vt): split Rust size report 14 writer

--- src/terminal/rust/size_report_write.rs
+++ src/terminal/rust/size_report_write.rs
@@ -3,6 +3,7 @@ use core::ffi::c_int;
 use crate::constants::*;
 use crate::size_report_len::*;
 use crate::size_report_pixels::*;
+use crate::size_report_write_14::*;
 use crate::size_report_write_2048::*;
 use crate::simple::*;
 
@@ -16,11 +17,7 @@ pub(crate) unsafe fn write_size_report(style: c_int, size: GhosttySizeReportSize
             write_size_report_2048(size, out);
         },
         SIZE_REPORT_CSI_14_T => unsafe {
-            write_bytes(out, &mut offset, b"\x1b[4;");
-            write_decimal(out, &mut offset, height_pixels(size));
-            write_bytes(out, &mut offset, b";");
-            write_decimal(out, &mut offset, width_pixels(size));
-            write_bytes(out, &mut offset, b"t");
+            write_size_report_14(size, out);
         },
         SIZE_REPORT_CSI_16_T => unsafe {
             write_bytes(out, &mut offset, b"\x1b[6;");
---
+++ src/terminal/rust/size_report_write_14.rs
@@ -0,0 +1,14 @@
+use crate::constants::*;
+use crate::size_report_pixels::*;
+use crate::simple::*;
+
+pub(crate) unsafe fn write_size_report_14(size: GhosttySizeReportSize, out: *mut u8) {
+    let mut offset = 0usize;
+    unsafe {
+        write_bytes(out, &mut offset, b"\x1b[4;");
+        write_decimal(out, &mut offset, height_pixels(size));
+        write_bytes(out, &mut offset, b";");
+        write_decimal(out, &mut offset, width_pixels(size));
+        write_bytes(out, &mut offset, b"t");
+    }
+}
