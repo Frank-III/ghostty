9a2d86f1e058b30c4d9b878c7d1c48f07374d4d2
Frank Wang <1454884738@qq.com>
2026-05-26 01:54:10 -0700

    refactor(vt): split Rust render row set ABI shims

--- src/terminal/rust/render_row_set.rs
+++ src/terminal/rust/render_row_set.rs
@@ -5,6 +5,10 @@ use crate::constants::*;
 
 #[no_mangle]
 pub unsafe extern "C" fn ghostty_rust_render_row_set_dirty(value: bool, out: *mut bool) -> c_int {
+    unsafe { render_row_set_dirty_impl(value, out) }
+}
+
+pub(crate) unsafe fn render_row_set_dirty_impl(value: bool, out: *mut bool) -> c_int {
     if out.is_null() {
         return RENDER_RESULT_INVALID_VALUE;
     }
@@ -18,6 +22,15 @@ pub unsafe extern "C" fn ghostty_rust_render_row_set_dirty(value: bool, out: *mu
 
 #[no_mangle]
 pub extern "C" fn ghostty_rust_render_row_set(
+    has_iterator: bool,
+    has_row: bool,
+    option: c_int,
+    has_value: bool,
+) -> c_int {
+    render_row_set_impl(has_iterator, has_row, option, has_value)
+}
+
+pub(crate) fn render_row_set_impl(
     has_iterator: bool,
     has_row: bool,
     option: c_int,
