2287c360235f0a9a62272454e35dca9066295106
Frank Wang <1454884738@qq.com>
2026-05-25 22:47:47 -0700

    refactor(vt): split Rust terminal string setter

---
+++ src/terminal/rust/terminal_set_string.rs
@@ -0,0 +1,36 @@
+use core::ffi::c_int;
+use core::ptr;
+
+use crate::constants::*;
+use crate::early::*;
+use crate::simple::*;
+use crate::terminal::*;
+
+#[no_mangle]
+pub unsafe extern "C" fn ghostty_rust_terminal_set_string(
+    value: *const GhosttyString,
+    out_ptr: *mut *const u8,
+    out_len: *mut usize,
+) -> c_int {
+    if out_ptr.is_null() || out_len.is_null() {
+        return GHOSTTY_INVALID_VALUE;
+    }
+
+    let (ptr, len) = if value.is_null() {
+        (EMPTY_UTF8.as_ptr(), 0)
+    } else {
+        unsafe {
+            (
+                ptr::read(core::ptr::addr_of!((*value).ptr)),
+                ptr::read(core::ptr::addr_of!((*value).len)),
+            )
+        }
+    };
+
+    unsafe {
+        ptr::write(out_ptr, ptr);
+        ptr::write(out_len, len);
+    }
+
+    GHOSTTY_SUCCESS
+}
