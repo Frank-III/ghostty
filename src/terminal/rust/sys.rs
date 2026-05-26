c6df4b88be84b6f66e4dbe8fa00cb3c21549c332
Frank Wang <1454884738@qq.com>
2026-05-26 01:35:06 -0700

    refactor(vt): split Rust sys option ABI shim

--- src/terminal/rust/sys.rs
+++ src/terminal/rust/sys.rs
@@ -8,6 +8,10 @@ pub(crate) const SYS_OPT_LOG: c_int = 2;
 
 #[no_mangle]
 pub extern "C" fn ghostty_rust_sys_set(option: c_int) -> c_int {
+    sys_set_impl(option)
+}
+
+pub(crate) fn sys_set_impl(option: c_int) -> c_int {
     match option {
         SYS_OPT_USERDATA | SYS_OPT_DECODE_PNG | SYS_OPT_LOG => GHOSTTY_SUCCESS,
         _ => GHOSTTY_INVALID_VALUE,
